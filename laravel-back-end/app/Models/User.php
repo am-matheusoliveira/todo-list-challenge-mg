0ae7365110ab78cbb40bd80bc64d0c38c3927f0a
Matheus Oliveira Pereira <sap.matheusoliveira@gmail.com>
2025-04-09 22:08:16 -0300

    feat: implementa autenticação com JWT no back-end Laravel
    Criação do arquivo api.php, nova migration para guardar os tekens do sanctum, e criação de Controller para as funcionalidades do usuário(Login, Logout, Register)

--- laravel-back-end/app/Models/User.php
+++ laravel-back-end/app/Models/User.php
@@ -6,8 +6,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
@@ -45,6 +46,26 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+    
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 
     /*
         RELACIONAMENTO COM A TABELA TASK.
@@ -53,5 +74,5 @@ class User extends Authenticatable
     public function tasks()
     {
         return $this->hasMany(Task::class, 'user_id', 'id');
-    }    
+    }
 }
